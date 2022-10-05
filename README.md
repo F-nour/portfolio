17858d90c9cbbb00e020aad3f64c02a76a9c2642
Nour-eddine <git@github.com>
2022-10-05 14:07:21 +0200

    initialisation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+
+Création d'un portfolio avec mon cv et mes réalisations en utilisant React et TypeScript.
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Layout from './Layout'
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
